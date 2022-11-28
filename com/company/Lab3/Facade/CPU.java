1ca7178bd14f3467ae61e6146f3a48bba2c28c0f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-28 21:11:09 +0200

    Added memory class

--- com/company/Lab3/Facade/CPU.java
+++ com/company/Lab3/Facade/CPU.java
@@ -4,7 +4,10 @@ public class CPU {
     public void freeze(){
         System.out.println("Freeze processor");
     }
-    public void jeep(){
-        System.out.println("");
+    public void jump(){
+        System.out.println("Processor jump to another process");
+    }
+    public void execute(){
+        System.out.println("Processor execute something");
     }
 }
---
+++ com/company/Lab3/Facade/Memory.java
@@ -0,0 +1,4 @@
+package com.company.Lab3.Facade;
+
+public class Memory {
+}
