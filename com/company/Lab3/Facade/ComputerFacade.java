61ee6b41b824ff3cbf8821f147a3a3cfb8cd3ac5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-28 21:21:16 +0200

    Created class ComputerFacade/method start added

---
+++ com/company/Lab3/Facade/ComputerFacade.java
@@ -0,0 +1,20 @@
+package com.company.Lab3.Facade;
+
+public class ComputerFacade {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public ComputerFacade(CPU cpu, Memory memory, HardDrive hardDrive) {
+        this.cpu = cpu;
+        this.memory = memory;
+        this.hardDrive = hardDrive;
+    }
+    public void start(){
+        cpu.freeze();
+        memory.load();
+        cpu.execute();
+        hardDrive.read();
+        cpu.jump();
+    }
+}
--- com/company/Lab3/Facade/HardDrive.java
+++ com/company/Lab3/Facade/HardDrive.java
@@ -1,7 +1,7 @@
 package com.company.Lab3.Facade;
 
 public class HardDrive {
-    public void load(){
+    public void read(){
         System.out.println("Hdd read some data");
     }
 }
