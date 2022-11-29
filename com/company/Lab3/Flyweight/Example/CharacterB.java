348ee737547dae2a37530917b08069b22e5ee7cd
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-29 20:20:38 +0200

    Flyweight char b

---
+++ com/company/Lab3/Flyweight/Example/CharacterB.java
@@ -0,0 +1,14 @@
+package com.company.Lab3.Flyweight.Example;
+
+public class CharacterB extends EnglishCharacter{
+    public CharacterB() {
+        symbol = 'B';
+        width = 15;
+        height = 25;
+    }
+
+    @Override
+    protected void print() {
+        System.out.println("\nSymbol: "  + symbol + "\nwidth: " + width + "\nheight: " + height);
+    }
+}
